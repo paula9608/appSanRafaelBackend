b1494f191a0f3733d712d1e30966fd3655239689
USER <user@MacBook-Pro-van-USER.local>
2019-06-16 13:24:21 -0500

    sinc

--- src/main/java/com/uniquindio/sanrafael/DatabaseConfig.java
+++ src/main/java/com/uniquindio/sanrafael/DatabaseConfig.java
@@ -1,10 +1,16 @@
 package com.uniquindio.sanrafael;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
@@ -12,13 +18,26 @@ import com.zaxxer.hikari.HikariDataSource;
 @Configuration
 public class DatabaseConfig {
 
+    Logger log = LoggerFactory.getLogger(getClass());
+
+
   @Value("${spring.datasource.url}")
   private String dbUrl;
-
+  
+  @Value("${spring.datasource.username}")
+  private String user;
+  
+  @Value("${spring.datasource.password}")
+  private String password;
+  
   @Bean
   public DataSource dataSource() {
       HikariConfig config = new HikariConfig();
       config.setJdbcUrl(dbUrl);
+      config.setUsername(user);
+      config.setPassword(password);
       return new HikariDataSource(config);
   }
+  
+  
 }
